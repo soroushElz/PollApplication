f816bde3ff48f4c9d02adf29112ea6f2c4be7889
Soroush <Soroush.yz.97@gmail.com>
2026-08-15 01:54:47 +0330

    Delete IdeaProjects/polls directory

--- IdeaProjects/polls/src/main/java/com/example/polls/Payload/VoteResponse.java
+++
@@ -1,4 +0,0 @@
-package com.example.polls.Payload;
-
-public record VoteResponse(Long voteId,Long PollId,UserSummary voter,String choice) {
-}
--- IdeaProjects/polls/src/main/java/com/example/polls/models/RoleName.java
+++
@@ -1,7 +0,0 @@
-package com.example.polls.models;
-
-public enum RoleName {
-
-	 ROLE_USER,
-	    ROLE_ADMIN
-}
